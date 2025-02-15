550231a470b8d57d1f96a270c3b78e84c6876db9
Yu Jie Teo <yujie@protonmail.com>
2025-02-15 14:56:22 +0800

    feat: updated vector library

--- vec3.h
+++ vec3.h
@@ -2,4 +2,25 @@
 
 #define VEC3_H
 
+#include <cmath>
+#include <iostream>
+
+class vec3 {
+	public:
+		// orthonormal vector
+		double e[3];
+		
+		vec3(): e{0,0,0} {}
+		vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+		// return x, y, z coordinates
+		double x() const { return e[0]; }
+		double y() const { return e[1]; }
+		double z() const { return e[2]; }
+
+		// operator for --
+		vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
+	};
+
 #endif
