f338241bf0e37f909e7d2ab567eeb62cb3c607b0
Yu Jie Teo <yujie@protonmail.com>
2025-02-16 17:11:25 +0800

    feat: add hittable headers

--- hittable.h
+++ hittable.h
@@ -8,6 +8,15 @@ class hit_record {
 		point3 p;
 		vec3 normal;
 		double t;
+		bool front_face;
+
+		void set_face_normal(const ray&r, const vec3& outward_normal) {
+			// sets hit record normal vector.
+			// IMPORTANT THE OUTWARD NORMAL IS ASSUMED TO HAVE UNTI LENGTH!!!
+
+			front_face = dot(r.direction(), outward_normal) < 0;
+			normal = front_face ? outward_normal : -outward_normal;
+		}
 };
 
 // surface is not used, since volumes will be considered.
--- sphere.h
+++ sphere.h
@@ -39,7 +39,9 @@ class sphere : public hittable {
 		// what on earth this is doing, is it calculating some sort of tangency?
 		rec.t = root;
 		rec.p = r.at(rec.t);
-		rec.normal = (rec.p - center) / radius;
+		// rec.normal = (rec.p - center) / radius;
+		vec3 outward_normal = (rec.p - center) / radius;
+		rec.set_face_normal(r, outward_normal);
 
 		// this checks that the thing is being done.
 		return true;
