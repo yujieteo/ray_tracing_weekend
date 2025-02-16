d1bf3465f920f96c9c06dc414e9aba16297090c4
Yu Jie Teo <yujie@protonmail.com>
2025-02-16 17:01:30 +0800

    feat: added relevant sphere class

--- main.cc
+++ main.cc
@@ -8,8 +8,74 @@
 
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+	
+	// this turns the sphere into a vector
+	vec3 oc = center - r.origin();
+
+	// sphere is represented by a quadratic form, solution to a quadratic equation
+	// auto a = dot(r.direction(), r.direction());
+	auto a = r.direction().length_squared();
+
+	// auto b = -2.0 * dot(r.direction(), oc);
+	auto h = dot(r.direction(), oc);
+
+	auto c = oc.length_squared() - radius*radius;
+
+	// auto discriminant = b*b - 4*a*c;
+
+	auto discriminant = h*h - a*c;
+
+	// test for camera front and back.
+	if (discriminant < 0) {
+		return -1.0;
+	} else {
+		return (h - std::sqrt(discriminant)) / (a);
+		// this is the quadratic equation
+		// substitute the b = -2h, 
+		// return (-b - std::sqrt(discriminant)) / (2.0 * a);
+	}
+
+	return (discriminant >= 0);
+}
+
+/*
+// Original implementation of hit sphere to create a blue sphere.
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	
+	// this turns the sphere into a vector
+	vec3 oc = center - r.origin();
+
+	// sphere is represented by a quadratic form, solution to a quadratic equation
+	auto a = dot(r.direction(), r.direction());
+
+	auto b = -2.0 * dot(r.direction(), oc);
+
+	auto c = dot(oc, oc) - radius*radius;
+
+	auto discriminant = b*b - 4*a*c;
+
+	return (discriminant >= 0);
+}
+*/
+
 color ray_color(const ray& r) {
+
+	// coloring based on normal vectors.
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+		return 0.5*color(N.x()+1, N.y()+1, N.z() + 1);
+	}
+
 	vec3 unit_direction = unit_vector(r.direction());
+
+	if (hit_sphere(point3(0,0,-1), 0.5, r)) {
+		// try to use curly braces
+		// I wanted a blue sphere.
+		return color(0,0,1);
+	}
 
 	auto a  = 0.5 * (unit_direction.y() + 1.0);
 
---
+++ sphere.h
@@ -0,0 +1,54 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+	public:
+	// don't understand this line, this returns the max of two arguments?
+	// is it because there is a lack of orthogonality in the code?
+		sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+
+	bool hit(const ray&r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+		// set up the centre properly, note that initially it was affine.
+		vec3 oc = center - r.origin();
+
+		// same abstraction for quadratic forms
+		auto a = r.direction().length_squared();
+		auto h = dot(r.direction(), oc);
+		auto c = oc.length_squared() - radius*radius;
+		auto discriminant = h*h - a*c;
+
+		if (discriminant < 0) {
+			return false;
+		}
+
+		auto sqrtd = std::sqrt(discriminant);
+
+		auto root = (h - sqrtd) / a;
+
+		if (root <= ray_tmin || ray_tmax <= root) {
+			root = (h + sqrtd) / a;
+			if (root <= ray_tmin || ray_tmax <= root) {
+				return false;
+			}
+		}
+
+
+		// what on earth this is doing, is it calculating some sort of tangency?
+		rec.t = root;
+		rec.p = r.at(rec.t);
+		rec.normal = (rec.p - center) / radius;
+
+		// this checks that the thing is being done.
+		return true;
+	}
+
+	private:
+
+	point3 center;
+	double radius;
+};
+
+#endif
